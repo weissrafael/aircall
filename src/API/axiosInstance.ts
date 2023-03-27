bcdb922f7786a345f3253d39f626c0ffe3069510
weissrafael <dataweiss@gmail.com>
2023-03-26 22:10:36 -0300

    Testing deployment with hard coded url

--- src/API/axiosInstance.ts
+++ src/API/axiosInstance.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL:
+    'https://charming-bat-singlet.cyclic.app/https://cerulean-marlin-wig.cyclic.app/',
   withCredentials: false,
   headers: {
     Accept: 'application/json',
